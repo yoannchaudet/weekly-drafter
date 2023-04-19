cfdcef3de61d925182d4392382d210943d2f0925
Yoann Chaudet <yoannchaudet@github.com>
2023-04-19 11:32:36 -0700

    scaffold a basic dotnet app

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using CommandLine;
+
+Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
+{
+  if (o.Verbose)
+  {
+    Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+    Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+  }
+  else
+  {
+    Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+    Console.WriteLine("Quick Start Example!");
+  }
+});
+
+public class Options
+{
+  [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+  public bool Verbose { get; set; }
+}
