f438b1d5787cabd5ae5bc24e04ed384572e9da8c
Yoann Chaudet <yoannchaudet@github.com>
2023-04-21 21:49:15 -0700

    cosmetic

--- src/WeeklyDrafter/ActionContext.cs
+++ src/WeeklyDrafter/ActionContext.cs
@@ -1,13 +1,9 @@
 public class ActionContext
 {
   // Default variables from https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
-
   public string GitHubToken => GetFromEnvironment("GITHUB_TOKEN", true)!;
-
   public string GitHubRepository => GetFromEnvironment("GITHUB_REPOSITORY", true)!;
-
   public string GitHubRepositoryOwnerName => GitHubRepository.Split("/")[0];
-
   public string GitHubRepositoryName => GitHubRepository.Split("/")[1];
 
   // Extract a value from the environment variable.
