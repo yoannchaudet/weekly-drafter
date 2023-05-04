33252ad40b0ba8a6adf960a1bbc3020adfe7d4d6
Yoann Chaudet <yoannchaudet@github.com>
2023-05-03 21:23:53 -0700

    Add basic logger

--- src/WeeklyDrafter.Tests/Utils/LoggerTests.cs
+++ src/WeeklyDrafter.Tests/Utils/LoggerTests.cs
@@ -1,19 +1,105 @@
 public class LoggerTests
 {
-  // CommandEnvelope test
+  // Logging tests
+
+  [Fact]
+  public void testDebug()
+  {
+    var output = new StringWriter();
+    Console.SetOut(output);
+    Logger.Debug("hello");
+    Assert.Equal($"::debug::hello{Environment.NewLine}", output.ToString());
+  }
+
+  [Fact]
+  public void testError()
+  {
+    var output = new StringWriter();
+    Console.SetOut(output);
+    Logger.Error("hello");
+    Assert.Equal($"::error::hello{Environment.NewLine}", output.ToString());
+  }
+
+  [Fact]
+  public void testErrorWithAnnotation()
+  {
+    var output = new StringWriter();
+    Console.SetOut(output);
+    Logger.Error("hello", new Logger.AnnotationProperties { File = "foo", StartLine = "1", EndLine = "2", StartColumn = "3", EndColumn = "4" });
+    Assert.Equal($"::error title=,file=foo,line=1,endLine=2,col=3,endColumn=4::hello{Environment.NewLine}", output.ToString());
+  }
+
+  [Fact]
+  public void testWarning()
+  {
+    var output = new StringWriter();
+    Console.SetOut(output);
+    Logger.Warning("hello");
+    Assert.Equal($"::warning::hello{Environment.NewLine}", output.ToString());
+  }
+
+  [Fact]
+  public void testWarningWithAnnotation()
+  {
+    var output = new StringWriter();
+    Console.SetOut(output);
+    Logger.Warning("hello", new Logger.AnnotationProperties { File = "foo", StartLine = "1", EndLine = "2", StartColumn = "3", EndColumn = "4" });
+    Assert.Equal($"::warning title=,file=foo,line=1,endLine=2,col=3,endColumn=4::hello{Environment.NewLine}", output.ToString());
+  }
+
+  [Fact]
+  public void testNotice()
+  {
+    var output = new StringWriter();
+    Console.SetOut(output);
+    Logger.Notice("hello");
+    Assert.Equal($"::warning::hello{Environment.NewLine}", output.ToString());
+  }
+
+  [Fact]
+  public void testNoticeWithAnnotation()
+  {
+    var output = new StringWriter();
+    Console.SetOut(output);
+    Logger.Notice("hello", new Logger.AnnotationProperties { File = "foo", StartLine = "1", EndLine = "2", StartColumn = "3", EndColumn = "4" });
+    Assert.Equal($"::warning title=,file=foo,line=1,endLine=2,col=3,endColumn=4::hello{Environment.NewLine}", output.ToString());
+  }
+
+  [Fact]
+  public void testInfo()
+  {
+    var output = new StringWriter();
+    Console.SetOut(output);
+    Logger.Info("hello");
+    Assert.Equal($"hello{Environment.NewLine}", output.ToString());
+  }
+
+  [Fact]
+  public void testGroup()
+  {
+    var output = new StringWriter();
+    Console.SetOut(output);
+    using (Logger.Group("hello"))
+    {
+      Logger.Info("world");
+    }
+    Assert.Equal($"::group name=hello::{Environment.NewLine}world{Environment.NewLine}::endgroup::{Environment.NewLine}", output.ToString());
+  }
+
+  // CommandEnvelope tests
 
   [Fact]
   public void CommandEnvelopeToString()
   {
     var envelope = new Logger.CommandEnvelope("test", new Dictionary<string, string> { { "foo", "bar" } }, "hello");
-    Assert.Equal("::test foo=bar hello", envelope.ToString());
+    Assert.Equal("::test foo=bar::hello", envelope.ToString());
   }
 
   [Fact]
   public void CommandEnvelopeNoParametersToString()
   {
     var envelope = new Logger.CommandEnvelope("test", null, "hello");
-    Assert.Equal("::test hello", envelope.ToString());
+    Assert.Equal("::test::hello", envelope.ToString());
   }
 
   [Fact]
@@ -21,13 +107,13 @@ public class LoggerTests
   {
     // probably invalid but we'll allow it
     var envelope = new Logger.CommandEnvelope("test", null, "");
-    Assert.Equal("::test", envelope.ToString());
+    Assert.Equal("::test::", envelope.ToString());
   }
 
   [Fact]
   public void CommandEnvelopeParametersEscapingToString()
   {
     var envelope = new Logger.CommandEnvelope("test", new Dictionary<string, string> { { "a", "b:" }, { "c", "d," } }, "\r\n%hello%");
-    Assert.Equal("::test a=b%3A,c=d%2C %0D%0A%25hello%25", envelope.ToString());
+    Assert.Equal("::test a=b%3A,c=d%2C::%0D%0A%25hello%25", envelope.ToString());
   }
 }
--- src/WeeklyDrafter/Utils/Logger.cs
+++ src/WeeklyDrafter/Utils/Logger.cs
@@ -1,39 +1,109 @@
 using System.Collections.ObjectModel;
-using System.Collections.Immutable;
 using System.Text;
 
 public static class Logger
 {
-  // WIP
+  // Write a debug message
+  public static void Debug(string message)
+  {
+    IssueCommand(new CommandEnvelope("debug", null, message));
+  }
 
+  // Write an error message (potentially along with an annotation)
+  public static void Error(string message, AnnotationProperties? annotationProps = null)
+  {
+    IssueCommand(new CommandEnvelope("error", annotationProps.ToDictionary(), message));
+  }
 
+  // Write a warning message (potentially along with an annotation)
+  public static void Warning(string message, AnnotationProperties? annotationProps = null)
+  {
+    IssueCommand(new CommandEnvelope("warning", annotationProps.ToDictionary(), message));
+  }
+
+  // Write a notice message (potentially along with an annotation)
+  public static void Notice(string message, AnnotationProperties? annotationProps = null)
+  {
+    IssueCommand(new CommandEnvelope("notice", annotationProps.ToDictionary(), message));
+  }
+
+  // Write an info message
+  public static void Info(string message)
+  {
+    Console.WriteLine(message);
+  }
+
+  // Write a group[ and return an IDisposable to be used in a using statement (for closing the group)
+  public static IDisposable Group(string name)
+  {
+    IssueCommand(new CommandEnvelope("group", new Dictionary<string, string> { { "name", name } }, ""));
+    return new EndGroupDisposable();
+  }
+
+  // Issue a command on the standard output
+  private static void IssueCommand(CommandEnvelope command)
+  {
+    Console.WriteLine(command.ToString());
+  }
+
+  // Disposable responsible for closing a group
+  private class EndGroupDisposable : IDisposable
+  {
+    public void Dispose()
+    {
+      IssueCommand(new CommandEnvelope("endgroup", null, ""));
+    }
+  }
+
+  // Convert an annotation properties object to a dictionary
+  private static Dictionary<string, string> ToDictionary(this AnnotationProperties? annotationProps)
+  {
+    return annotationProps == null ? new Dictionary<string, string>() : new Dictionary<string, string> {
+      { "title", annotationProps.Title ?? "" },
+      { "file", annotationProps.File ?? "" },
+      { "line", annotationProps.StartLine ?? "" },
+      { "endLine", annotationProps.EndLine ?? "" },
+      { "col", annotationProps.StartColumn ?? "" },
+      { "endColumn", annotationProps.EndColumn ?? "" }
+    };
+  }
+
+  // Annotation properties object
+  public class AnnotationProperties
+  {
+    public string? Title { get; set; }
+    public string? File { get; set; }
+    public string? StartLine { get; set; }
+    public string? EndLine { get; set; }
+    public string? StartColumn { get; set; }
+    public string? EndColumn { get; set; }
+  }
 
   // Envelope objects for console commands
   public class CommandEnvelope
   {
     public string Command { get; }
-    public ReadOnlyDictionary<string, string> Parameters { get; }
+    public ReadOnlyDictionary<string, string>? Parameters { get; }
     public string Message { get; }
 
     public CommandEnvelope(string command, Dictionary<string, string>? parameters, string message)
     {
       Command = command;
-      // TODO: in .net 8, use ReadOnlyDictionary.Empty
-      Parameters = parameters == null ? new Dictionary<string, string>().AsReadOnly() : parameters.AsReadOnly();
+      Parameters = parameters?.AsReadOnly();
       Message = message;
     }
 
     // Output the envelope as a string
-    public String ToString()
+    public override String ToString()
     {
       var builder = new StringBuilder();
       builder.Append($"::{Command}");
-      if (Parameters.Count > 0)
+      if (Parameters?.Count > 0)
       {
         builder.Append(" ");
         builder.AppendJoin(",", Parameters.Select(p => $"{EscapeProperty(p.Key)}={EscapeProperty(p.Value)}"));
       }
-      builder.Append(" ");
+      builder.Append("::");
       builder.Append(EscapeData(Message));
       return builder.ToString().TrimEnd();
     }
